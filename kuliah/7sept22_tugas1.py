8e68699aa33c1c9e30ca8b9de86458f33cb23979
Sulthan Abiyyu <zblackjack980@gmail.com>
2022-09-07 22:02:55 +0700

    update plotting

--- kuliah/7sept22_tugas1.py
+++ kuliah/7sept22_tugas1.py
@@ -1,4 +1,3 @@
-from cProfile import label
 import math
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -41,10 +40,15 @@ if __name__ == "__main__":
 
     sn_1 = un_1_df["Sn"]
     Er_1 = []
+    kurva_galat_sebenarnya_1 = []
     for i in range(len(un_1_df)):
         if i == 0:
+            kurva_galat_sebenarnya_1.append(
+                abs((sn_1[i] - true_value_1)/true_value_1 * 100))
             Er_1.append(None)
         else:
+            kurva_galat_sebenarnya_1.append(
+                abs((sn_1[i] - true_value_1)/true_value_1 * 100))
             Er_1.append(abs((sn_1[i] - sn_1[i-1]) / (sn_1[i]) * 100))
 
     un_1_df["Galat Aproksimasi"] = Er_1
@@ -65,10 +69,15 @@ if __name__ == "__main__":
 
     sn_2 = un_2_df["Sn"]
     Er_2 = []
+    kurva_galat_sebenarnya_2 = []
     for i in range(len(un_2_df)):
         if i == 0:
+            kurva_galat_sebenarnya_2.append(
+                abs((sn_2[i] - true_value_1)/true_value_1 * 100))
             Er_2.append(None)
         else:
+            kurva_galat_sebenarnya_2.append(
+                abs((sn_2[i] - true_value_1)/true_value_1 * 100))
             Er_2.append(
                 abs((sn_2[i] - sn_2[i-1]) / (sn_2[i]) * 100))
 
@@ -79,7 +88,8 @@ if __name__ == "__main__":
     print(f"Galat sebenarnya: {galat_sebenarnya_2}")
 
     # Plotting
-    fig, ax = plt.subplots(2, 2, figsize=(10, 10))
+    fig, ax = plt.subplots(2, 3, figsize=(15, 10))
+    fig.suptitle("Latihan Soal Bab Aproksimasi Galat", fontsize=20)
 
     ax[0, 0].title.set_text("Fungsi Cosinus dan Aproksimasinya")
     ax[0, 0].plot(un_1_df["Sn"], label="Aproksimasi")
@@ -89,6 +99,9 @@ if __name__ == "__main__":
     ax[0, 1].title.set_text("Galat Aproksimasi No. 1")
     sns.lineplot(data=un_1_df, x=un_1_df.index,
                  y="Galat Aproksimasi", ax=ax[0, 1])
+    ax[0, 2].title.set_text("Kurva Galat Sebenarnya No. 1")
+    sns.lineplot(data=un_1_df, x=un_1_df.index,
+                 y=kurva_galat_sebenarnya_1, ax=ax[0, 2])
 
     ax[1, 0].title.set_text("Fungsi Sinus dan Aproksimasinya")
     ax[1, 0].plot(un_2_df["Sn"], label="Aproksimasi")
@@ -98,4 +111,8 @@ if __name__ == "__main__":
     ax[1, 1].title.set_text("Galat Aproksimasi No. 2")
     sns.lineplot(data=un_2_df, x=un_2_df.index,
                  y="Galat Aproksimasi", ax=ax[1, 1])
+    ax[1, 2].title.set_text("Kurva Galat Sebenarnya No. 2")
+    sns.lineplot(data=un_2_df, x=un_2_df.index,
+                 y=kurva_galat_sebenarnya_2, ax=ax[1, 2])
+
     plt.show()
