0c19dcdd6243dff1147f2386b6aef656421cb192
SulthanAbiyyu <zblackjack980@gmail.com>
2022-09-23 12:17:37 +0700

    bug cause: plateau

--- Persamaan Non Linear/titik_potong/naive_iteration_step.py
+++ Persamaan Non Linear/titik_potong/naive_iteration_step.py
@@ -6,15 +6,17 @@ def naive_iteration_step(f, g, x0, tol=0.1, max_iter=1000, step=0.1):
         else:
             if abs((f(xi) - g(xi))) > abs(f(xi + step) - g(xi + step)):
                 xi += step
-            else:
+            elif abs(f(xi) - g(xi)) > abs(f(xi - step) - g(xi - step)):
                 xi -= step
+            else:
+                print("plateau")
     return xi
 
 if __name__ == "__main__":
-    f = lambda x: x ** 2 + 1
+    f = lambda x: x ** 2 - 1
     g = lambda x: x ** 3
 
-    x_tipot = naive_iteration_step(f, g, 1)
+    x_tipot = naive_iteration_step(f, g, 0)
     print(x_tipot)
     print(f(x_tipot))
     print(g(x_tipot))
