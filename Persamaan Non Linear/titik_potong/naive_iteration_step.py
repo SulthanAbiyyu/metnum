618515667dc4b78a5901085ff66e36d0a9c55737
Sulthan Abiyyu <zblackjack980@gmail.com>
2022-09-23 14:45:54 +0700

    added patience count

--- Persamaan Non Linear/titik_potong/naive_iteration_step.py
+++ Persamaan Non Linear/titik_potong/naive_iteration_step.py
@@ -1,9 +1,14 @@
 import numpy as np
 
 
-def naive_iteration_step(f, g, x0, tol=0.1, max_iter=1000, step=0.1):
+def naive_iteration_step(f, g, x0, tol=0.1, max_iter=1000, step=0.1, patience=10):
     xi = x0
+    not_updated = 0
     for i in range(max_iter):
+        if not_updated > patience:
+            print("Wrong initialization")
+            break
+
         if abs(f(xi) - g(xi)) < tol:
             break
         else:
@@ -12,22 +17,33 @@ def naive_iteration_step(f, g, x0, tol=0.1, max_iter=1000, step=0.1):
             elif abs(max(f(xi), g(xi)) - min(f(xi), g(xi))) > abs(max(f(xi - step), g(xi - step)) - min(f(xi - step), g(xi - step))):
                 xi -= step
             else:
-                print(f"plateau at {i + 1} th iteration")
+                not_updated += 1
+                # print(f"""
+                # plateau at {i + 1} th iteration
+                # xi              : {xi}
+                # step            : {step}
+                # f(x)            : {f(xi)}
+                # g(x)            : {g(xi)}
+                # Initial point   : {abs(max(f(xi), g(xi)) - min(f(xi), g(xi)))}
+                # Step up point   : {abs(max(f(xi + step), g(xi + step)) - min(f(xi + step), g(xi + step)))}
+                # Step down point : {abs(max(f(xi - step), g(xi - step)) - min(f(xi - step), g(xi - step)))}
+                # """)
+
     return xi
 
 
 def biyu_initialization(f, g, tries=3, range=(-10, 10)):
     x = np.random.randint(range[0], range[1], size=(tries))
-
     closest_point = [0, 9999]  # index, min_val
     for idx, xi in enumerate(x):
+
         y_max = max(f(xi), g(xi))
         y_min = min(f(xi), g(xi))
         length = abs(y_max - y_min)
 
         if length < closest_point[1]:
             closest_point[0] = idx
-            closest_point[1] = xi
+            closest_point[1] = length
 
     return x[closest_point[0]]
 
@@ -36,8 +52,9 @@ if __name__ == "__main__":
     def f(x): return x ** 2 - 1
     def g(x): return x ** 3
 
+    np.random.seed(22)
     x0 = biyu_initialization(f, g, tries=10)
-    x_tipot = naive_iteration_step(f, g, -0.5, max_iter=1_000_000, step=1e-5)
-    print(x_tipot)
-    print(f(x_tipot))
-    print(g(x_tipot))
+    x_tipot = naive_iteration_step(f, g, x0, max_iter=1_000, step=1e-3)
+
+    print(
+        f"x0 = {x0} \t\t x_tipot = {x_tipot: .3f} \nf(x) = {f(x_tipot): .3f} \t g(x) = {g(x_tipot): .3f}")
