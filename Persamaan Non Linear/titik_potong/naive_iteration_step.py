d6379cdc2c5dc9e460e584b1284fb3dddce77bfd
SulthanAbiyyu <zblackjack980@gmail.com>
2022-09-23 11:03:31 +0700

    naive tipot, bug when x0 = 0

---
+++ Persamaan Non Linear/titik_potong/naive_iteration_step.py
@@ -0,0 +1,20 @@
+def naive_iteration_step(f, g, x0, tol=0.1, max_iter=1000, step=0.1):
+    xi = x0
+    for _ in range(max_iter):
+        if abs(f(xi) - g(xi)) < tol:
+            break
+        else:
+            if abs((f(xi) - g(xi))) > abs(f(xi + step) - g(xi + step)):
+                xi += step
+            else:
+                xi -= step
+    return xi
+
+if __name__ == "__main__":
+    f = lambda x: x ** 2 + 1
+    g = lambda x: x ** 3
+
+    x_tipot = naive_iteration_step(f, g, 1)
+    print(x_tipot)
+    print(f(x_tipot))
+    print(g(x_tipot))
