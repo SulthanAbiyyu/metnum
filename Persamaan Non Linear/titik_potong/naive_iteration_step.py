d0fb81281f56aad80446bc22f84731477f0f5379
Sulthan Abiyyu <zblackjack980@gmail.com>
2022-09-23 13:59:11 +0700

    bug fix

--- Persamaan Non Linear/titik_potong/naive_iteration_step.py
+++ Persamaan Non Linear/titik_potong/naive_iteration_step.py
@@ -1,22 +1,24 @@
 def naive_iteration_step(f, g, x0, tol=0.1, max_iter=1000, step=0.1):
     xi = x0
-    for _ in range(max_iter):
+    for i in range(max_iter):
         if abs(f(xi) - g(xi)) < tol:
             break
         else:
-            if abs((f(xi) - g(xi))) > abs(f(xi + step) - g(xi + step)):
+            if abs(max(f(xi), g(xi)) - min(f(xi), g(xi))) > abs(max(f(xi + step), g(xi + step)) - min(f(xi + step), g(xi + step))):
                 xi += step
-            elif abs(f(xi) - g(xi)) > abs(f(xi - step) - g(xi - step)):
+            elif abs(max(f(xi), g(xi)) - min(f(xi), g(xi))) > abs(max(f(xi - step), g(xi - step)) - min(f(xi - step), g(xi - step))):
                 xi -= step
             else:
-                print("plateau")
+                print(f"plateau at {i + 1} th iteration")
     return xi
 
-if __name__ == "__main__":
-    f = lambda x: x ** 2 - 1
-    g = lambda x: x ** 3
+def initialization
 
-    x_tipot = naive_iteration_step(f, g, 0)
+if __name__ == "__main__":
+    def f(x): return x ** 2 - 1
+    def g(x): return x ** 3
+
+    x_tipot = naive_iteration_step(f, g, -0.5, max_iter=1_000_000, step=1e-5)
     print(x_tipot)
     print(f(x_tipot))
     print(g(x_tipot))
