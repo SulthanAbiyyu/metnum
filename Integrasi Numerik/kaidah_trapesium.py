52bb7ddb9b7e6c776fa3db8d7fc8d40f818d562f
Sulthan Abiyyu <zblackjack980@gmail.com>
2022-09-24 13:44:52 +0700

    bug fix trapesium

--- Integrasi Numerik/kaidah_trapesium.py
+++ Integrasi Numerik/kaidah_trapesium.py
@@ -7,23 +7,18 @@ def kaidah_trapesium(f, a, b, n):
     n: banyaknya strip
     """
 
-    lebar = (b-a)/ n
-    xi = a
-    integral = f(a) + f(b)
+    lebar = (b-a) / n
+    integral = 0.5 * (f(a) + f(b))
     sigma = 0
 
-    for _ in range(1, b):
-        xi += lebar
-        sigma += 2 * f(xi)
+    for i in range(1, n):
+        sigma += f(a + i * lebar)
 
-    integral = (integral + sigma) * lebar / 2
-    
-    
-    print((sigma + integral) * lebar)
+    integral = (integral + sigma) * lebar
 
     return integral
 
-if __name__ == "__main__":
-    f = lambda x: x ** 2 + 1
-    print(kaidah_trapesium(f, 0, 2, 10))
 
+if __name__ == "__main__":
+    def f(x): return x ** 2 + 1
+    print(kaidah_trapesium(f, 0, 2, 10))
