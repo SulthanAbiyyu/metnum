59f027e7973f02314350a64225a6ed26451156bf
Sulthan Abiyyu <zblackjack980@gmail.com>
2022-10-05 21:25:57 +0700

    power

--- eigen/power_method.py
+++ eigen/power_method.py
@@ -4,17 +4,19 @@ import numpy as np
 
 def power_iteration(A: np.ndarray, x0: np.array, n_iter=100):
     x_new = x0
-    for _ in range(n_iter):
+    for i in range(n_iter):
         dott = np.dot(A, x_new)
         d = dott[np.argmax(np.abs(dott))]
         x_new = dott / d
-    return x_new
+        print(f"i: {i + 1} | d: {d} | dott: {dott} | x: {x_new}")
+    return d, x_new
 
 
 if __name__ == "__main__":
     A = np.array([
-        [5, -2],
-        [7, -4]
+        [2, 1, 2],
+        [1, 3, 2],
+        [2, 4, 1]
     ])
-    x0 = np.array([1, 3])
-    print(power_iteration(A, x0, n_iter=28))
+    x0 = np.array([1, 1, 1])
+    print(power_iteration(A, x0, n_iter=8))
--- eigen/qr_decomposition.py
+++ eigen/qr_decomposition.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def gram_schmidt(A: np.ndarray, normalized=True) -> np.ndarray:
     V = np.zeros(A.shape)
     V[:, 0] = A[:, 0]
@@ -7,14 +8,16 @@ def gram_schmidt(A: np.ndarray, normalized=True) -> np.ndarray:
     for col in range(1, A.shape[1]):
         summa = 0
         for w in range(col):
-            summa += (A[:, col] @ V[:, w]) / (np.linalg.norm(V[:, w]) ** 2) * V[:, w]
+            summa += (A[:, col] @ V[:, w]) / \
+                (np.linalg.norm(V[:, w]) ** 2) * V[:, w]
         V[:, col] = A[:, col] - summa
-    
+
     if normalized:
         for col in range(V.shape[1]):
             V[:, col] = V[:, col] / np.linalg.norm(V[:, col])
-    
+
     return V
+
 
 def qr_decompose(A, normalized=True):
     Q = gram_schmidt(A, normalized)
@@ -22,19 +25,24 @@ def qr_decompose(A, normalized=True):
 
     return Q, R
 
+
 def qr_eigen_iteration(A, n_iter=100, normalized=True):
     Q, R = qr_decompose(A, normalized)
     A_baru = np.zeros(A.shape)
     for _ in range(n_iter):
         A_baru = R @ Q
         Q, R = qr_decompose(A_baru)
-    
+
     return A_baru
+
 
 if __name__ == "__main__":
     A = np.array([
-        [5, -2],
-        [7, -4],
+        [2, 1, 2],
+        [1, 3, 4],
+        [2, 2, 1]
     ])
     q, r = qr_decompose(A)
-    print(qr_eigen_iteration(A))
+    print(q)
+    print(r)
+    # print(qr_eigen_iteration(A))
