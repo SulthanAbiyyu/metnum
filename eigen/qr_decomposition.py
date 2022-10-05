772d2aafe1113b50551859f983259130db57518f
Sulthan Abiyyu <zblackjack980@gmail.com>
2022-10-05 20:54:40 +0700

    qr

--- eigen/qr_decomposition.py
+++ eigen/qr_decomposition.py
@@ -7,22 +7,34 @@ def gram_schmidt(A: np.ndarray, normalized=True) -> np.ndarray:
     for col in range(1, A.shape[1]):
         summa = 0
         for w in range(col):
-            summa += (A[:, col] @ V[:, w]) / np.linalg.norm(V[:, w]) * V[:, w]
+            summa += (A[:, col] @ V[:, w]) / (np.linalg.norm(V[:, w]) ** 2) * V[:, w]
         V[:, col] = A[:, col] - summa
     
     if normalized:
         for col in range(V.shape[1]):
-            V[:, col] = V[:, col] / np.linalg.norm(V[:, col] ** 2)
+            V[:, col] = V[:, col] / np.linalg.norm(V[:, col])
     
     return V
 
+def qr_decompose(A, normalized=True):
+    Q = gram_schmidt(A, normalized)
+    R = Q.T @ A
+
+    return Q, R
+
+def qr_eigen_iteration(A, n_iter=100, normalized=True):
+    Q, R = qr_decompose(A, normalized)
+    A_baru = np.zeros(A.shape)
+    for _ in range(n_iter):
+        A_baru = R @ Q
+        Q, R = qr_decompose(A_baru)
+    
+    return A_baru
+
 if __name__ == "__main__":
     A = np.array([
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9]
+        [5, -2],
+        [7, -4],
     ])
-
-    V = gram_schmidt(A)
-
-    print(np.linalg.norm(V[:, 0]))
+    q, r = qr_decompose(A)
+    print(qr_eigen_iteration(A))
