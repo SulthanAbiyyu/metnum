41568c9d41efc101970084e5b1521366fd07cdf0
Sulthan Abiyyu <zblackjack980@gmail.com>
2022-10-06 09:21:19 +0700

    power inverse iter

--- eigen/power_method.py
+++ eigen/power_method.py
@@ -12,6 +12,16 @@ def power_iteration(A: np.ndarray, x0: np.array, n_iter=100):
     return d, x_new
 
 
+def power_inverse_iteration(A: np.ndarray, x0: np.array, n_iter=100):
+    x_new = x0
+    for i in range(n_iter):
+        dott = np.dot(A, x_new)
+        d = dott[np.argmin(np.abs(dott))]
+        x_new = dott / d
+        print(f"i: {i + 1} | d: {d} | dott: {dott} | x: {x_new}")
+    return d, x_new
+
+
 if __name__ == "__main__":
     A = np.array([
         [2, 1, 2],
@@ -19,4 +29,4 @@ if __name__ == "__main__":
         [2, 4, 1]
     ])
     x0 = np.array([1, 1, 1])
-    print(power_iteration(A, x0, n_iter=8))
+    print(power_inverse_iteration(A, x0, n_iter=8))
