aade152860788c0f17a9c5e6e890e1eb1b41b17a
Sulthan Abiyyu <zblackjack980@gmail.com>
2022-12-06 07:13:01 +0700

    soal

--- Turunan Numerik/beda_tengah.py
+++ Turunan Numerik/beda_tengah.py
@@ -1,2 +1,9 @@
+import numpy as np
+
+
 def beda_tengah(f, x0, h):
     return (f(x0+h)-f(x0-h))/(2*h)
+
+
+if __name__ == "__main__":
+    print(beda_tengah(lambda x: np.sin(x)/x, 1, 0.5))
--- Turunan Numerik/turunan_ekstrapolasi_richardson.py
+++ Turunan Numerik/turunan_ekstrapolasi_richardson.py
@@ -1,2 +1,9 @@
+import numpy as np
+
+
 def turunan_ekstrapolasi_richardson(f, x0, h):
     return (8 * f(x0 + h) - 8 * f(x0 - h) - (f(x0 + 2*h) - f(x0 - 2*h))) / (12 * h)
+
+
+if __name__ == "__main__":
+    print(turunan_ekstrapolasi_richardson(lambda x: np.tan(x), 1, 0.5))
